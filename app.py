42c24a1df06565a3f7417c8bcaab04882f65cc63
Eduardo <duda@DESKTOP-MOCUNDL.localdomain>
2020-08-14 10:53:53 -0300

    1Primeira versão de análise de relatório

--- app.py
+++ app.py
@@ -2,79 +2,86 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+
+
+#df = pd.read_excel("/mnt/c/Users/lemmo/Google Drive/Auditoria Fiscal/Operacional/Análises/Reg.xlsx")
+df = pd.read_excel("./Reg.xlsx")
+
+df['CODIGO'] = df['CODIGO'].astype(str)
+df['RETENÇÃO NA FONTE'] = df['RETENÇÃO NA FONTE'].replace(np.nan,'Completa a tabela com o pdf original')
+def resultado(df1, idx):    
+    string = ''
+    if len(df.index)!= 0:
+        string += '\nServiço: ' + df1['TITULO'].iloc[idx]
+        string += '\n\nExceção na LC 116?: ' + df1['EXCECAO'].iloc[idx]
+        string += '\n\nLocal da Prestação: ' + df1['LOCALDAPRESTACAOPJ'].iloc[idx]
+        string += '\n\nBase de Cálculo: ' + df1['BASEDECALCULO'].iloc[idx]
+        string += '\n\nDedução da Base de Cálculo: ' + df1['DEDUCAODABASEDECALCULO'].iloc[idx]
+        string += '\n\nRetenção na Fonte: ' + df1['RETENÇÃO NA FONTE'].iloc[idx]
+
+    else:
+        return 'Codigo não encontrado'
+    return string
+
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.write("""
-# Penguin Prediction App
+# FISCO UTILS
+Relatórios inteligentes
 
-This app predicts the **Palmer Penguin** species!
-
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
-st.sidebar.header('User Input Features')
+st.sidebar.header('Aqui é o Header')
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+#st.sidebar.markdown("""
+#[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+#""")
 
 # Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+uploaded_file = st.sidebar.file_uploader("Faça o Upload", type=["xlsx"])
 if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
+    input_df = pd.read_excel(uploaded_file)
+    
+    alvo = input_df
+    st.write(input_df)
+   
+    filtro = ['Nº Sequencial', 'Cidade Tomador','Data Emissão','Valor Documento', 'Valor Tributável', 'Atividade',
+          'ITEM LC 116/2003', 'Imposto Retido', 'TOTAL_IMPOSTO',
+        'Tributado Município', 'Retido','Descrição dos Serviços']
+    alvo = alvo[filtro]
+    alvo_tomador_caxias = alvo[alvo['Cidade Tomador']=='Duque de Caxias']
+
+    def loopa(data):
+        for mes in range(1,13):
+            
+            alvo_mensal = alvo[alvo['Data Emissão'].dt.month == mes]
+            
+            if alvo_mensal.empty == False:
+                st.write('############################################################################')
+                st.write('No mês {} o contribuinte emitiu {}  nota(s) relativa(s) a {} código(s) de atividade(s):'.format(mes,len(alvo_mensal['Nº Sequencial']),alvo_mensal['Atividade'].nunique()))
+                #atividades = alvo['Atividade'].unique()
+                atividades = alvo_mensal['Atividade'].unique()
+                for atividade in atividades:
+                    st.write('{}'.format(atividade[0:5]),end='\n\n')
+
+                st.write(alvo_mensal[['Atividade','Valor Tributável']].groupby('Atividade').sum())
+                st.write('############################################################################\n\n')
+            
+    loopa(alvo)
+
+    atividades = alvo['Atividade'].unique()
+    for atividade in atividades:
+        code = atividade[0:5]
+        idx = df.loc[df['CODIGO'] == code].index.values[0]
+        st.write('\n')
+        st.write('CÓDIGO: {}'.format(code))
+        st.write(resultado(df,idx))
 else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+    st.write('Carregue o arquivo - Relatório de Notas fiscais Emitidas pelo contribuinte para análise')
+    #st.write(input_df)
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
-
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
-
-# Displays the user input features
-st.subheader('User Input features')
-
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
-
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
-
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+#input_df.columns
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
